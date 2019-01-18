5f3ba9e08b2718903424ecf6d64d125f2277fe44
In-Cheol Shin <incheol.shin97@gmail.com>
2019-01-18 18:08:59 +0900

    create main.cpp
    implement file I/O

---
+++ src/main.cpp
@@ -0,0 +1,66 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2019. 1. 18.
+ *      Author: Max Shin
+ */
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <string>
+#include <Vector>
+#include <random>
+
+using namespace std;
+
+class WordSet{
+private:
+	pair<string, string>* words;
+public:
+	WordSet(){
+		words = new pair<string, string>[40];
+	}
+
+	void insertWords(string eng, string kor, int idx){
+		words[idx] = make_pair(eng, kor);
+	}
+
+	pair<string, string> getWord(int idx){
+		return words[idx];
+	}
+
+	~WordSet(){
+		free(words);
+	}
+};
+
+int main(){
+	int week = 0;
+	cout << "English Voca Quiz for TOEIC\n";
+	cout << "Version: 0.1\n";
+	cout << "--------------------------------------------------------\n";
+	cout << "Select Week: ";
+	cin >> week;
+	cout << "/wordset/day" + to_string(week) + ".txt";
+
+	ifstream inp;
+	inp.open("wordset/day" + to_string(week) + ".txt", ios::in);
+
+	WordSet* ws = new WordSet();
+	string eng;
+	string kor;
+	for(int i = 0; i < 20; i++){
+		getline(inp, eng);
+		getline(inp, kor);
+
+		ws->insertWords(eng, kor, i);
+	}
+
+	for(int i = 0; i < 20; i++){
+		pair<string, string> _pair = ws->getWord(i);
+
+		cout << _pair.first << " " << _pair.second  << endl;
+	}
+	return 0;
+}
