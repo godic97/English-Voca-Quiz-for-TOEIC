ee559a4bdb89f31a20e5d1b9edf7bd1c5f2e1621
In-Cheol Shin <incheol.shin97@gmail.com>
2019-01-18 19:06:06 +0900

    Update main.cpp
    check input, number or not

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,13 @@
 #include <fstream>
 #include <cstring>
 #include <string>
-#include <Vector>
 #include <random>
 #include <cstdlib>
+#include <cctype>
 
 using namespace std;
+
+char isdigit_loop();
 
 class WordSet{
 private:
@@ -37,16 +39,19 @@ public:
 };
 
 int main(){
-	int week = 0;
+	char day = 0;
 	cout << "English Voca Quiz for TOEIC\n";
 	cout << "Version: 0.1\n";
 	cout << "--------------------------------------------------------\n";
-	cout << "Select Week: ";
-	cin >> week;
-	cout << "/wordset/day" + to_string(week) + ".txt" << endl;
+	cout << "Select day: ";
+	day = isdigit_loop();
+
+
+	int day_i = (int)day-48;
+	cout << "/wordset/day" + to_string(day_i) + ".txt" << endl;
 
 	ifstream inp;
-	inp.open("./wordset/day" + to_string(week) + ".txt", ios::in);
+	inp.open("./wordset/day" + to_string(day_i) + ".txt", ios::in);
 
 	WordSet* ws = new WordSet();
 	string eng;
@@ -66,4 +71,17 @@ int main(){
 			 cout << _pair.second << endl;
 	}
 	return 0;
+}
+
+char isdigit_loop(){
+	char day = 0;
+	cout << "Select day: ";
+	cin >> day;
+
+	if(!isdigit(day)){
+		cout << "Only Number" << endl;
+		return isdigit_loop();
+	}
+
+	return day;
 }
